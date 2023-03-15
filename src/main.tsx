eb04bc1c857cefe0d8246051881c716907de4b7e
dannycahyo <dannydwicahyono@gmail.com>
2023-03-15 22:07:29 +0700

    adjust global styles into the chakra provider

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   styles: {
     global: {
+      body: {
+        background: "#091227",
+      },
       "h1, h2, h3, h4, h5, h6": {
         color: "#EAF0FF",
       },
